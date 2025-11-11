861b030992e0af105c469936566c6a77578a4cbc
BrahiamAc <brahiam.acosta@udea.edu.co>
2025-11-11 15:23:35 -0500

    habilitacion de todos los test con datos actualizados

--- src/test/java/org/udea/parabank/TestRunner.java
+++ src/test/java/org/udea/parabank/TestRunner.java
@@ -4,33 +4,33 @@ import com.intuit.karate.junit5.Karate;
 
 class TestRunner {
 
-    // @Karate.Test
-    // Karate test01_ParabankLogin() {
-    //     return Karate.run("login")
-    //             .relativeTo(getClass())
-    //             .outputCucumberJson(true);
-    // }
+    @Karate.Test
+    Karate test01_ParabankLogin() {
+        return Karate.run("login")
+                .relativeTo(getClass())
+                .outputCucumberJson(true);
+    }
 
-    // @Karate.Test
-    // Karate test02_ParabankAccounts() {
-    //     return Karate.run("accounts")
-    //             .relativeTo(getClass())
-    //             .outputCucumberJson(true);
-    // }
+    @Karate.Test
+    Karate test02_ParabankAccounts() {
+        return Karate.run("accounts")
+                .relativeTo(getClass())
+                .outputCucumberJson(true);
+    }
     
-    // @Karate.Test
-    // Karate test03_ParabankTransfer() {
-    //     return Karate.run("transfer")
-    //             .relativeTo(getClass())
-    //             .outputCucumberJson(true);
-    // }
+    @Karate.Test
+    Karate test03_ParabankTransfer() {
+        return Karate.run("transfer")
+                .relativeTo(getClass())
+                .outputCucumberJson(true);
+    }
 
-    // @Karate.Test
-    // Karate test04_ParabankBillPay() {
-    //     return Karate.run("billpay")
-    //             .relativeTo(getClass())
-    //             .outputCucumberJson(true);
-    // }
+    @Karate.Test
+    Karate test04_ParabankBillPay() {
+        return Karate.run("billpay")
+                .relativeTo(getClass())
+                .outputCucumberJson(true);
+    }
 
     @Karate.Test
     Karate test05_ParabankLoan() {
